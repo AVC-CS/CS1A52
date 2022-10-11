63c400dfc916eb463a7935f646cca1c7604dbe05
Kyuwoong Lee <kwooler@gmail.com>
2022-10-10 22:00:33 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Complete the following program. 
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	/* 
+	Complete this program
+	*/
+	
+	cout << setprecision(5) << fixed;
+	cout << "2 to Power " << n << " is " << pow << endl;
+	
+}
