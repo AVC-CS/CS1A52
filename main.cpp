627fc0bc36c80ed172b0f52ab32593c3f7ded377
Kyuwoong Lee <kwooler@gmail.com>
2022-10-10 22:02:58 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
-// Complete the following program. 
-#include <iostream>
-#include <iomanip>
-
-using namespace std;
+#include        <iostream>
+#include        <iomanip>
+using   namespace       std;
 
 int main()
 {
-	/* 
-	Complete this program
-	*/
-	
-	cout << setprecision(5) << fixed;
-	cout << "2 to Power " << n << " is " << pow << endl;
-	
+    int min, number;
+
+	// complete your code
+  
+    cout << "The min value is " << min << endl;
 }
