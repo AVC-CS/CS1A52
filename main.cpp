e22ed3c9d019c472669d19e976e06071611821ad
Kyuwoong Lee <kwooler@gmail.com>
2022-12-10 15:37:40 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,1 @@
-#include        <iostream>
-#include        <iomanip>
-using   namespace       std;
-
-int main()
-{
-    int min, number;
-
-	// complete your code
-  
-    cout << "The min value is " << min << endl;
-}
+// Make your code here
