0731f04d4ed4ad6d6685625a9f0e98542498b04b
Blake Mosley <bmosley2@students.avc.edu>
2024-03-11 12:41:03 -0700

    done

--- main.cpp
+++ main.cpp
@@ -2,9 +2,20 @@
 
 int main()
 {
-  
-/* ************************************************
-	// complete your code
-	*********************************************** */
+
+	// take 10 user inputs on 10 lines then find largest
+	int largest = 0;
+	int input = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << "Enter a number: ";
+		std::cin >> input;
+		if (input > largest)
+		{
+			largest = input;
+		}
+	}
+	std::cout << "The largest number is: " << largest << std::endl;
+	
 
 }
